a7afcdfec06dad97ab32aeeb4c9598ffc9ac84c5
Nguyen Duc Thinh <thinhnd194@gmail.com>
2022-06-08 22:16:29 +0700

    learn sending email with nodemailer

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,16 +1,27 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	auth: {
+		user: '0497b07d464ce4',
+		pass: 'bcc26eca7915d9',
+	},
+});
+
 exports.getLogin = (req, res, next) => {
-	let err = req.flash('error')
-	let message = null
+	let err = req.flash('error');
+	let message = null;
 	if (err.length > 0) {
-		message = err[0]
+		message = err[0];
 	}
 	res.render('auth/login', {
 		pageTitle: 'Login',
 		path: '/login',
 		isAuthenticated: req.isLoggedIn,
-		errorMessage: message
+		errorMessage: message,
 	});
 };
 
@@ -21,25 +32,25 @@ exports.postLogin = (req, res, next) => {
 	User.findOne({ email: email })
 		.then((user) => {
 			if (!user) {
-				req.flash('error', 'Invalid email or password!')
+				req.flash('error', 'Invalid email or password!');
 				return res.redirect('/login');
 			}
-			return bcrypt.compare(password, user.password).then(matched => {
-        if (matched) {
-          req.session.isLoggedIn = true
-          req.session.user = user
-          return req.session.save((err) => {
-						console.log(err)
-            return res.redirect('/')
-          })
-        }
-				req.flash('error', 'Invalid email or password!')
-        return res.redirect('/login')
-      })
+			return bcrypt.compare(password, user.password).then((matched) => {
+				if (matched) {
+					req.session.isLoggedIn = true;
+					req.session.user = user;
+					return req.session.save((err) => {
+						console.log(err);
+						return res.redirect('/');
+					});
+				}
+				req.flash('error', 'Invalid email or password!');
+				return res.redirect('/login');
+			});
 		})
 		.catch((err) => {
-      console.log(err)
-    });
+			console.log(err);
+		});
 };
 
 exports.getSignUp = (req, res, next) => {
@@ -72,7 +83,15 @@ exports.postSignUp = (req, res, next) => {
 					return newUser.save();
 				})
 				.then(() => {
+					const mailOptions = {
+						from: '"Example Team" <from@example.com>',
+						to: email,
+						subject: 'Nice Nodemailer test',
+						text: 'Hey there, it’s our first message sent with Nodemailer ;) ',
+						html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer',
+					};
 					res.redirect('/login');
+					return transport.sendMail(mailOptions);
 				});
 		})
 		.catch((err) => {
